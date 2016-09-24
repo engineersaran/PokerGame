78aa47d6b385941e3345c23753561763ef2d94eb
engineersaran <engineersaran@gmail.com>
2016-09-24 19:18:00 +0530

    Modified display winner format output. Added displayhand for each player

--- src/game/poker/PokerHandRank.java
+++ src/game/poker/PokerHandRank.java
@@ -237,46 +237,55 @@ public class PokerHandRank {
 		if(PokerHandEvaluator.isFlush(player1.getCardInHand()) && PokerHandEvaluator.isStraight(player1.getCardInHand())) {
 			
 			player1.setRank(getStraightFlushRank(player1.getCardInHand()));
+			player1.setDisplayHand("Straight flush");
 		}
 		// is Four of a kind?
 		else if(PokerHandEvaluator.isFourOfaKind(player1.getCardInHand())) {
 			
 			player1.setRank(getFourOfAKindRank(player1.getCardInHand()));
+			player1.setDisplayHand("Four of a kind");
 		}
 		// is Full House?
 		else if(PokerHandEvaluator.isFullHouse(player1.getCardInHand())) {
 			
 			player1.setRank(getFullHouseRank(player1.getCardInHand()));
+			player1.setDisplayHand("Full House");
 		}
 		// is Flush?
 		else if(PokerHandEvaluator.isFlush(player1.getCardInHand())) {
 			
 			player1.setRank(getFlushRank(player1.getCardInHand()));
+			player1.setDisplayHand("Flush");
 		}
 		// is Straight?
 		else if(PokerHandEvaluator.isStraight(player1.getCardInHand())) {
 			
 			player1.setRank(getStraightRank(player1.getCardInHand()));
+			player1.setDisplayHand("Straight");
 		}
 		// is Three of a Kind?
 		else if(PokerHandEvaluator.isThreeOfaKind(player1.getCardInHand())) {
 			
 			player1.setRank(getThreeOfAKindRank(player1.getCardInHand()));
+			player1.setDisplayHand("Three of a Kind");
 		}
 		// is Two Pairs?
 		else if(PokerHandEvaluator.isTwoPairs(player1.getCardInHand())) {
 			
 			player1.setRank(getTwoPairsRank(player1.getCardInHand()));
+			player1.setDisplayHand("Two Pairs");
 		}
 		// is Pair?
 		else if(PokerHandEvaluator.isPair(player1.getCardInHand())) {
 			
 			player1.setRank(getPairRank(player1.getCardInHand()));
+			player1.setDisplayHand("Pair");
 		}
 		else {
 			
 			// High Card
 			player1.setRank(getHighCardRank(player1.getCardInHand()));
+			player1.setDisplayHand("High Card");
 		}
 		
 		// Player 2 hand strength check
@@ -284,66 +293,83 @@ public class PokerHandRank {
 		if(PokerHandEvaluator.isFlush(player2.getCardInHand()) && PokerHandEvaluator.isStraight(player2.getCardInHand())) {
 			
 			player2.setRank(getStraightFlushRank(player2.getCardInHand()));
+			player2.setDisplayHand("Straight flush");
 		}
 		// is Four of a kind?
 		else if(PokerHandEvaluator.isFourOfaKind(player2.getCardInHand())) {
 			
 			player2.setRank(getFourOfAKindRank(player2.getCardInHand()));
+			player2.setDisplayHand("Four of a kind");
 		}
 		// is Full House?
 		else if(PokerHandEvaluator.isFullHouse(player2.getCardInHand())) {
 			
 			player2.setRank(getFullHouseRank(player2.getCardInHand()));
+			player2.setDisplayHand("Full House");
 			
 		}
 		// is Flush?
 		else if(PokerHandEvaluator.isFlush(player2.getCardInHand())) {
 			
 			player2.setRank(getFlushRank(player2.getCardInHand()));
+			player2.setDisplayHand("Flush");
 			
 		}
 		// is Straight?
 		else if(PokerHandEvaluator.isStraight(player2.getCardInHand())) {
 			
 			player2.setRank(getStraightRank(player2.getCardInHand()));
+			player2.setDisplayHand("Straight");
 			
 		}
 		// is Three of a Kind?
 		else if(PokerHandEvaluator.isThreeOfaKind(player2.getCardInHand())) {
 			
 			player2.setRank(getThreeOfAKindRank(player2.getCardInHand()));
+			player2.setDisplayHand("Three of a Kind");
 		}
 		// is Two Pairs?
 		else if(PokerHandEvaluator.isTwoPairs(player2.getCardInHand())) {
 			
 			player2.setRank(getTwoPairsRank(player2.getCardInHand()));
+			player2.setDisplayHand("Two Pairs");
 		}
 		// is Pair?
 		else if(PokerHandEvaluator.isPair(player2.getCardInHand())) {
 			
 			player2.setRank(getPairRank(player2.getCardInHand()));
+			player2.setDisplayHand("Pair");
 		}
 		else {
 			
 			// High Card
 			player2.setRank(getHighCardRank(player2.getCardInHand()));
+			player2.setDisplayHand("High Card");
 		}
+		
+		
+		System.out.println("\n*********************************************************\n");
 		
 		if(player1.getRank() > player2.getRank()) {
 			
-			System.out.println("Player1 wins");
-			System.out.println("Score: "+player1.getRank() );
+			System.out.println("\t Player1 wins, Hurray !!!!, Lets Party");
+			System.out.println("\t Win by: "+player1.getDisplayHand());
+			System.out.println("\t Score: "+player1.getRank());
+			System.out.println("\n\tPlayer2");
+			System.out.println("\t lost by: "+player2.getDisplayHand());
+			System.out.println("\t Score: "+player2.getRank());
 		}
 		else if(player2.getRank() > player1.getRank()) {
 			
-			System.out.println("Player2 wins");
-			System.out.println("Score: "+player2.getRank() );
-		}
-		else {
-			
-			System.out.println("Player1 Ties with Player 2");
+			System.out.println("\t Player2 wins, Hurray !!!!, Lets Party");
+			System.out.println("\t Win by: "+player2.getDisplayHand());
+			System.out.println("\t Score: "+player2.getRank());
+			System.out.println("\n\tPlayer1");
+			System.out.println("\t lost by: "+player1.getDisplayHand());
+			System.out.println("\t Score: "+player1.getRank());
 		}
 		
+		System.out.println("\n*********************************************************\n");
 	}
 	
 }
